4bbd4cb1dadfcc38dbe2c11179bb1f22fc519c40
Jaisman <jaismanjhinger@gmail.com>
2025-06-12 11:27:38 +0530

    bot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Dashboard from './components/Dashboard';
 import AICreditScoreInsights from "./components/Credit";
 import ProfilePage from "./components/Profile";
 import NGOPage from "./components/Ngo";
+import FinancialQuizPage from "./components/FinancialQuizPage";
+import EduChat from "./components/EduChat";
 import Navbar from "./components/Navbar";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 function App() {
@@ -19,6 +21,8 @@ function App() {
         <Route path="/profile" element={<ProfilePage/>}/>
         <Route path="/credit" element={<AICreditScoreInsights/>}/>
         <Route path="/ngo" element={<NGOPage/>}/>
+        <Route path="/educhat" element={<EduChat/>}/>
+        <Route path="/quiz" element={<FinancialQuizPage/>}/>
       </Routes>
      </BrowserRouter>
     </>
